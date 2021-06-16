f813d256abe9b8869254b472efcd4e258972707f
HCBR-PRo <85325988+HCBR-Pro@users.noreply.github.com>
2021-06-16 14:01:51 -0300

    Add files via upload

---
+++ King/donasi.js
@@ -0,0 +1,18 @@
+const donasi = () => { 
+	return `       
+  *[ _DONASI_ ]*
+
+⦿ *PULSA*
+BELUM TERSEDIA
+⦿ *GOPAY*
+ BELUM TERSEDIA
+⦿ *SUBSCRIBE*
+https://youtube.com/channel/UCHK9gIokua2zkYKYTbqAwMQ
+⦿ *KEJAR JAM TAYANG*
+https://youtu.be/FlNALBtJCoo
+
+Gunakan doank, kaga donasi lorr
+`
+}
+
+exports.donasi = donasi
---
+++ settings/eng.js
@@ -0,0 +1,413 @@
+
+exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangkau, role, per) => {
+	return`
+〘 USER 〙
+⦿ Register: ✓
+⦿ Prefix: ${prefix}
+⦿ Security: ${per}
+⦿ Rank: ${role}
+⦿ Versi Wa: 2.21.5.17
+⦿ Server: _Baileys_
+⦿ Nomor: wa.me/${sender.split("@")[0]}
+⦿ Uang Mu: Rp${uangkau}
+⦿ Xp: ${getLevelingXp(sender)}/${reqXp}
+⦿ Level: ${getLevelingLevel(sender)}
+⦿ User Register: ${_registered.length}
+
+〘 MRKINGPANUTANKU 〙
+⦿ *SUBSCRIBE CHANNEL MR KING*
+⦿ *GO 4000 JAM TAYANG*
+⦿ *BISMILLAH TAHUN INI UDAH MONETISASI*
+
+〘 FUN 〙
+⦿ *${prefix}sticker*
+⦿ *${prefix}stikergif* max.9detik
+⦿ *${prefix}︎shadow*
+⦿ *${prefix}cup*
+⦿ *${prefix}cup1*
+⦿ *${prefix}googlesearch*
+⦿ *${prefix}smoke*
+⦿ *${prefix}burnpaper*
+⦿ *${prefix}lovemessage*
+⦿ *${prefix}undergrass*
+⦿ *${prefix}itxt*
+⦿ *${prefix}attp*
+⦿ *${prefix}love*
+⦿ *${prefix}coffe*
+⦿ *${prefix}woodheart*
+⦿ *${prefix}flowerheart*
+⦿ *${prefix}woodenboard*
+⦿ *${prefix}darkneon*
+⦿ *${prefix}candlemug*
+⦿ *${prefix}lovemsg*
+⦿ *${prefix}mugflower*
+⦿ *${prefix}narutobanner*
+⦿ *${prefix}paperonglass*
+⦿ *${prefix}romancetext*
+⦿ *${prefix}shadowtext*
+⦿ *${prefix}coffecup*
+⦿ *${prefix}glowingneon*
+⦿ *${prefix}underwater*
+⦿ *${prefix}hpotter*
+⦿ *${prefix}woodblock*
+⦿ *${prefix}summer3d*
+⦿ *${prefix}tahta*
+⦿ *${prefix}wolfmetal*
+⦿ *${prefix}nature3d*
+⦿ *${prefix}underwater*
+⦿ *${prefix}golderrose*
+⦿ *${prefix}summernature*
+⦿ *${prefix}letterleaves*
+⦿ *${prefix}glowingneon*
+⦿ *${prefix}fallleaves*
+⦿ *${prefix}flamming*
+⦿ *${prefix}harrypotter*
+⦿ *${prefix}carvedwood*
+⦿ *${prefix}wetglass*
+⦿ *${prefix}multicolor3d*
+⦿ *${prefix}watercolor*
+⦿ *${prefix}luxurygold*
+⦿ *${prefix}galaxywallpaper*
+⦿ *${prefix}lighttext*
+⦿ *${prefix}beautifulflower*
+⦿ *${prefix}puppycute*
+⦿ *${prefix}royaltext*
+⦿ *${prefix}heartshaped*
+⦿ *${prefix}birthdaycake*
+⦿ *${prefix}galaxystyle*
+⦿ *${prefix}hologram3d*
+⦿ *${prefix}glossychrome*
+⦿ *${prefix}greenbush*
+⦿ *${prefix}metallogo*
+⦿ *${prefix}noeltext*
+⦿ *${prefix}glittergold*
+⦿ *${prefix}textcake*
+⦿ *${prefix}starsnight* 
+⦿ *${prefix}wooden3d*
+⦿ *${prefix}textbyname*
+⦿ *${prefix}writegalacy*
+⦿ *${prefix}galaxybat*
+⦿ *${prefix}snow3d*
+⦿ *${prefix}birthdayday*
+⦿ *${prefix}freefire*
+⦿ *${prefix}kuncitext*
+⦿ *${prefix}textdaun* 
+⦿ *${prefix}nulis*
+⦿ *${prefix}silktext*
+⦿ *${prefix}makequote*
+⦿ *${prefix}toimg*
+⦿ *${prefix}ocr*
+⦿ *${prefix}galaxstyle*
+⦿ *${prefix}jokerlogo*
+⦿ *${prefix}toxic*
+⦿ *${prefix}bloodfrosted*
+⦿ *${prefix}hororblood*
+⦿ *${prefix}halloween*
+⦿ *${prefix}firework*
+⦿ *${prefix}glitch*
+⦿ *${prefix}blackpink*
+⦿ *${prefix}goldplay*
+⦿ *${prefix}hologram*
+⦿ *${prefix}textbyname*
+⦿ *${prefix}herrypoter*
+⦿ *${prefix}metallogo*
+⦿ *${prefix}ttp*
+⦿ *${prefix}thunder*
+⦿ *${prefix}bokeh*
+⦿ *${prefix}strawberry*
+⦿ *${prefix}metaldark*
+⦿ *${prefix}kuncitext*
+⦿ *${prefix}textdaun*
+⦿ *${prefix}nulis*
+⦿ *${prefix}silktext*
+⦿ *${prefix}makequote*
+⦿ *${prefix}toimg*
+⦿ *${prefix}ocr*
+⦿ *${prefix}galaxstyle*
+⦿ *${prefix}jokerlogo*
+⦿ *${prefix}toxic*
+⦿ *${prefix}triggered*
+⦿ *${prefix}gtav*
+⦿ *${prefix}nigthbeach*
+⦿ *${prefix}laptop*
+⦿ *${prefix}linephoto*
+⦿ *${prefix}raindrop*
+⦿ *${prefix}sketch*
+⦿ *${prefix}wanted*
+⦿ *${prefix}crossgun*
+⦿ *${prefix}bloodfrosted*
+⦿ *${prefix}hororblood*
+⦿ *${prefix}halloween*
+⦿ *${prefix}firework*
+⦿ *${prefix}blackpink*
+⦿ *${prefix}goldplay*
+⦿ *${prefix}hologram*
+⦿ *${prefix}textbyname*
+⦿ *${prefix}herrypoter*
+⦿ *${prefix}imagetext*
+⦿ *${prefix}greenneon*
+⦿ *${prefix}metallogo*
+⦿ *${prefix}spamcall*
+⦿ *${prefix}spamtext*
+⦿ *${prefix}mining*
+⦿ *${prefix}playstore*
+⦿ *${prefix}bisakah*
+⦿ *${prefix}kapankah*
+⦿ *${prefix}apakah*
+⦿ *${prefix}rate*
+⦿ *${prefix}truth*
+⦿ *${prefix}dare*
+⦿ *${prefix}hobby*
+⦿ *${prefix}memeindo*
+⦿ *${prefix}darkjoke*
+⦿ *${prefix}cerpen*
+⦿ *${prefix}quotesimage*
+⦿ *${prefix}fitnah*
+⦿ *${prefix}pasangan*
+⦿ *${prefix}ntahlah*
+⦿ *${prefix}slap*
+⦿ *${prefix}quotesmotivasi*
+⦿ *${prefix}quotessehat*
+⦿ *${prefix}quotescinta*
+⦿ *${prefix}hemkel*
+⦿ *${prefix}quotes2*
+⦿ *${prefix}katadoi*
+⦿ *${prefix}katakatadilan*
+⦿ *${prefix}qoutes*
+⦿ *${prefix}caklontong*
+⦿ *${prefix}family100*
+⦿ *${prefix}tebakgambar*
+⦿ *${prefix}kbbi*
+⦿ *${prefix}dadu*
+⦿ *${prefix}artinama*
+
+〘 ANIME 〙
+⦿ *${prefix}animesaran*
+⦿ *${prefix}animesaran2*
+⦿ *${prefix}husbu2*
+⦿ *${prefix}anime*
+⦿ *${prefix}wallpaperanime*
+⦿ *${prefix}trap* 
+⦿ *${prefix}baka2*
+⦿ *${prefix}wallpapernime*
+⦿ *${prefix}animefanart*
+⦿ *${prefix}megumin*
+⦿ *${prefix}shinobu*
+⦿ *${prefix}baka*
+⦿ *${prefix}eroyuri*
+⦿ *${prefix}happy*
+⦿ *${prefix}dance*
+⦿ *${prefix}neko3*
+⦿ *${prefix}smile*
+⦿ *${prefix}wallpaper*
+⦿ *${prefix}slapnime*
+⦿ *${prefix}shota*
+⦿ *${prefix}sagiri*
+⦿ *${prefix}femdom*
+⦿ *${prefix}waifukawai*
+⦿ *${prefix}kuni*
+⦿ *${prefix}nsfwloli3*
+⦿ *${prefix}kitsune*
+⦿ *${prefix}yuri*
+⦿ *${prefix}yaoi*
+⦿ *${prefix}wancak*
+⦿ *${prefix}quotesnime*
+⦿ *${prefix}waifu2*
+⦿ *${prefix}bj* (udh work)
+⦿ *${prefix}ram*
+⦿ *${prefix}pictlolicon*
+⦿ *${prefix}pictneko*
+⦿ *${prefix}testwaifu*
+⦿ *${prefix}nsfw_avatar*
+⦿ *${prefix}senku*
+⦿ *${prefix}pictwaifu*
+⦿ *${prefix}kurumi2*
+⦿ *${prefix}nakanomiku*
+⦿ *${prefix}wibu*
+⦿ *${prefix}wibu2*
+⦿ *${prefix}boruto*
+⦿ *${prefix}rize*
+⦿ *${prefix}kaneki*
+⦿ *${prefix}kemonomimi*
+⦿ *${prefix}holo*
+⦿ *${prefix}naruto*
+⦿ *${prefix}amv*
+⦿ *${prefix}minato*                                                                          
+⦿ *${prefix}tagall*
+⦿ *${prefix}gecg*
+⦿ *${prefix}avatar*
+⦿ *${prefix}miku*
+⦿ *${prefix}kurumi*
+⦿ *${prefix}hinata*
+⦿ *${prefix}sasuke*
+⦿ *${prefix}sakura*
+⦿ *${prefix}akura*
+⦿ *${prefix}itori*
+⦿ *${prefix}touka*
+⦿ *${prefix}rem*
+⦿ *${prefix}chika*
+
+〘 OWNER 〙	
+⦿ *${prefix}bc* 
+⦿ *${prefix}bcgc* *ERROR!*
+⦿ *${prefix}setlimit*
+⦿ *${prefix}setreply*
+⦿ *${prefix}setprefix*
+⦿ *${prefix}clearall 1*
+⦿ *${prefix}ban*
+⦿ *${prefix}setshizuka*
+⦿ *${prefix}setnamebot*
+⦿ *${prefix}settobz*
+⦿ *${prefix}addlimit*
+⦿ *${prefix}setvhtear*
+⦿ *${prefix}setlolhuman*
+⦿ *${prefix}setzeks*
+⦿ *${prefix}setahmad*
+⦿ *${prefix}unban*
+⦿ *${prefix}block*
+⦿ *${prefix}unblock*
+⦿ *${prefix}setmemberlimit*
+⦿ *${prefix}addbadword* <teks>
+⦿ *${prefix}listbadword*
+⦿ *${prefix}nobadword*
+⦿ *${prefix}listblock*
+⦿ *${prefix}leave*
+⦿ *${prefix}event* [1/0]
+⦿ *${prefix}clone*
+⦿ *${prefix}setbotpp*
+
+〘 MEDIA 〙		
+⦿ *${prefix}beritahoax*
+⦿ *${prefix}brainly*
+⦿ *${prefix}pinterest*
+⦿ *${prefix}imgurl*
+⦿ *${prefix}husbu*
+⦿ *${prefix}waifu2*
+⦿ *${prefix}waifu*
+⦿ *${prefix}loli*
+⦿ *${prefix}loli2*
+⦿ *${prefix}neko*
+⦿ *${prefix}neko2*
+⦿ *${prefix}nekonime*
+⦿ *${prefix}randomanime*
+⦿ *${prefix}randomhusbu*
+⦿ *${prefix}randomcyberspace*
+⦿ *${prefix}randomexo*
+⦿ *${prefix}blackpink*
+⦿ *${prefix}randomgame*
+⦿ *${prefix}randommountain*
+⦿ *${prefix}randomloli*
+⦿ *${prefix}randomprogramer*
+⦿ *${prefix}meme*
+⦿ *${prefix}memeindo*
+⦿ *${prefix}tts*
+⦿ *${prefix}play*
+⦿ *${prefix}lirik*
+⦿ *${prefix}ssweb*
+⦿ *${prefix}map*
+⦿ *${prefix}stalkig*
+⦿ *${prefix}afk*
+⦿ *${prefix}unafk*
+⦿ *${prefix}asupan*
+⦿ *${prefix}bitly*
+⦿ *${prefix}cuttly*	
+⦿ *${prefix}quran*
+⦿ *${prefix}quotesislami*
+⦿ *${prefix}listsurah*
+
+〘 GROUP 〙
+⦿ *${prefix}hidetag*
+⦿ *${prefix}del*
+⦿ *${prefix}jadian*
+⦿ *${prefix}ngewe*
+⦿ *${prefix}grouplist*
+⦿ *${prefix}level*
+⦿ *${prefix}leaderboard*
+⦿ *${prefix}linkgc*
+⦿ *${prefix}tagall*
+⦿ *${prefix}setpp*
+⦿ *${prefix}add*
+⦿ *${prefix}kick*
+⦿ *${prefix}setname*
+⦿ *${prefix}setdesc*
+⦿ *${prefix}demote*
+⦿ *${prefix}promote*
+⦿ *${prefix}listadmin*
+⦿ *${prefix}closegc*
+⦿ *${prefix}opengc*
+⦿ *${prefix}leveling*
+⦿ *${prefix}nsfw* [1/0]
+⦿ *${prefix}simih* [1/0]
+⦿ *${prefix}welcome* [1/0]
+⦿ *${prefix}antilinkgrup* [1/0]
+⦿ *${prefix}antivirtex* [1/0]	
+
+〘 OTHER 〙
+⦿ *${prefix}setprefix*
+⦿ *${prefix}daftar nama|umur*
+⦿ *${prefix}bugreport [teks]*
+⦿ *${prefix}listblock*
+⦿ *${prefix}listbanned*
+⦿ *${prefix}listgroup*
+⦿ *${prefix}info*
+⦿ *${prefix}lapor*
+⦿ *${prefix}request*
+⦿ *${prefix}limit*
+⦿ *${prefix}buylimit*
+⦿ *${prefix}dompet*
+⦿ *${prefix}rules*
+⦿ *${prefix}donasi*
+⦿ *${prefix}ping*
+
+〘 NSFW 〙			
+⦿ *${prefix}pokemon*
+⦿ *${prefix}anjing*
+⦿ *${prefix}nsfwloli*
+⦿ *${prefix}nsfwneko*
+⦿ *${prefix}solo*
+⦿ *${prefix}nsfwtrap*
+⦿ *${prefix}nsfwpussy*
+⦿ *${prefix}nsfwyuri*
+⦿ *${prefix}ero*
+⦿ *${prefix}nsfwloli2*
+⦿ *${prefix}sideoppai*
+⦿ *${prefix}nsfwwaifu*
+⦿ *${prefix}ecchi*
+⦿ *${prefix}nekopoi* <judul>
+
+〘 SOUND 〙
+⦿ *${prefix}sound1*
+⦿ *${prefix}sound2*
+⦿ *${prefix}sound3*
+⦿ *${prefix}sound4*
+⦿ *${prefix}sound5*
+⦿ *${prefix}sound6*
+⦿ *${prefix}sound7*
+⦿ *${prefix}sound8*
+⦿ *${prefix}sound9*
+⦿ *${prefix}sound10*
+⦿ *${prefix}sound11*
+⦿ *${prefix}sound12*
+⦿ *${prefix}sound13*
+⦿ *${prefix}sound14*
+⦿ *${prefix}sound15*
+⦿ *${prefix}sound16*
+⦿ *${prefix}sound17*
+⦿ *${prefix}sound18*
+⦿ *${prefix}sound19*
+⦿ *${prefix}sound20*
+⦿ *${prefix}sound21*
+⦿ *${prefix}sound22*
+⦿ *${prefix}sound23*
+⦿ *${prefix}sound24*
+⦿ *${prefix}sound25*
+⦿ *${prefix}djpota*
+
+〘 THANKS TO〙
+⦿ *JOHN*
+⦿ *YT SIWN*
+⦿ *IDDO*
+⦿ *MR KING*
+〘 KING-BOTZ 〙
+`
+}
